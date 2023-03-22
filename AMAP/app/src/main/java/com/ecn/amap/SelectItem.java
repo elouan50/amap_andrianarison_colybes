373cd898f9721401410f940add93b9c2fddf9972
elisa2306 <116555240+elisa2306@users.noreply.github.com>
2023-03-22 15:02:38 +0100

    button image

---
+++ AMAP/app/src/main/java/com/ecn/amap/SelectItem.java
@@ -0,0 +1,15 @@
+package com.ecn.amap;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_bread);
+    }
+}
