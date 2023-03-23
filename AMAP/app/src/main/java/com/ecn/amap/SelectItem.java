8f43be010088fa0c61a9b0747fe172c7f86da1b9
elouan50 <118377913+elouan50@users.noreply.github.com>
2023-03-23 10:49:43 +0100

    recup date

--- AMAP/app/src/main/java/com/ecn/amap/SelectItem.java
+++ AMAP/app/src/main/java/com/ecn/amap/SelectItem.java
@@ -11,12 +11,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SelectItem extends AppCompatActivity {
     private static final int TEXT_REQUEST = 1;
+    private static final int REQUEST_CODE_DATE_PICKER = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Date livraison = new Date();
+        Date today = new Date();
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_DATE_PICKER) {
+            String selectedDate = data.getStringExtra("selected_date");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                livraison = dateFormat.parse(selectedDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Commande commande = new Commande(today, livraison, 1);
+        int commande_id = commande.getCommmande_id();
+        System.out.println(commande_id);
     }
 
     @SuppressLint("RestrictedApi")
